5d8b8f166ef2772a0faf9409e86389321edb604d
kelvin onumajuru <kelvinonumajuru@gmail.com>
2022-11-29 07:34:12 -0800

    lowercase character

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) >= ord('97') and ord(c) <= ord('122'):
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
         return True
     else:
         return False
