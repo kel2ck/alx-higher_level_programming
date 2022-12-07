a14be5d37ced9d1a49033f7a11b489002d63ab39
kelvin onumajuru <kelvinonumajuru@gmail.com>
2022-12-07 05:30:00 -0800

    update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def update_dictionary(my_dictionary, key, value):
-    my_dictionary[key] = value
-    return my_dictionary
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
