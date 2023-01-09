eebccb0e63e8071b3d013e67cafd72e4c9ff891f
kelvin onumajuru <kelvinonumajuru@gmail.com>
2023-01-09 07:28:26 -0800

    my integer

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""
-Defines a class MyInt that inherits from int.
-
-"""
+"""Defines a class MyInt that inherits from int."""
 
 
 class MyInt(int):
-    """A custom integer class."""
+    """Invert int operators == and !=."""
 
-    def __ne__(self, x):
-        """Return True if x is equal to self."""
-        return int(self) == x
+    def __eq__(self, value):
+        """Override == opeartor with != behavior."""
+        return self.real != value
 
-    def __eq__(self, x):
-        """Return True if x is not equal to self."""
-        return int(self) != 
+    def __ne__(self, value):
+        """Override != operator with == behavior."""
+        return self.real == value
