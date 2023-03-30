c9613cc2766056bb578c8a13c828d6dd51b22e3b
kelvin onumajuru <kelvinonumajuru@gmail.com>
2023-03-30 12:03:05 -0700

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Take url, send request, display size of body
-curl -s "$1" | wc -c
-
+# Takes in a URL, sends a request to that URL, and display the size of the body of the response
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
