ac416c0009f0bfae31dc54a4188c07cd06461026
kelvin onumajuru <kelvinonumajuru@gmail.com>
2023-03-31 04:53:51 -0700

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A script that
+- fetches a URL.
+- uses urlib package
+"""
+
+from urllib.request import urlopen
+if __name__ == "__main__":
+    with urlopen("https://alx-intranet.hbtn.io/status") as response:
+        body = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode('utf-8')}"
