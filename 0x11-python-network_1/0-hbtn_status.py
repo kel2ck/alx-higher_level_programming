6c2ba57bdc3ab17ba92ceaa57ee0437381f5dcc5
kelvin onumajuru <kelvinonumajuru@gmail.com>
2023-03-31 05:03:44 -0700

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""A script that
-- fetches a URL.
-- uses urlib package
-"""
+"""Fetches a URL."""
+import urllib.request as request
 
-from urllib.request import urlopen
+
 if __name__ == "__main__":
-    with urlopen("https://alx-intranet.hbtn.io/status") as response:
-        body = response.read()
-        print("Body response:")
-        print(f"\t- type: {type(body)}")
-        print(f"\t- content: {body}")
-        print(f"\t- utf8 content: {body.decode('utf-8')}"
+    with request.urlopen("https://intranet.hbtn.io/status") as response:
+        if response.readable():
+            data = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(data)))
+            print("\t- content: {}".format(data))
+            print("\t- utf8 content: {}".format(data.decode("utf-8")))
